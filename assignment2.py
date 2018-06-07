549bce1ab50c94002997f4e7a9bb555c49145deb
manpreet01multani <manpreet01multani@gmail.com>
2018-06-07 02:02:54 -0700

    assignment 2

---
+++ assignment2.py
@@ -0,0 +1,22 @@
+# 1. print on screen
+print("hi I am manpreet")
+
+# 2. concatenation
+print("Acad"+"View")
+
+# 3. taking input printing its value
+#x = input("enter the value: ")
+#y = input("enter the value: ")
+#z = input("enter the value: ")
+#print("x is: "+x)
+#print("y is: "+y)
+#print("z is: "+z)
+
+# 4 print lets get started on screen
+#print('Let\'s get started')
+
+#print the following value using placeholders
+s= 'Acadview'
+course = 'Python'
+fees= 5000
+print("%s is offering %s @ %d" %(s,course,fees))

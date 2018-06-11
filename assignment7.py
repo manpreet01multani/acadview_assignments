14bef871fa58e832e5d8adb394c33fddf2c15c66
manpreet01multani <manpreet01multani@gmail.com>
2018-06-11 02:02:48 -0700

    assignment7

--- assignment7.py
+++ assignment7.py
@@ -20,33 +20,39 @@ print("Result:",power(base,exp))
 
 #find factorial and store in dictionary
 
-
-def factorial(number):
-    product=1
-    for i in range(number):
-        product=product*(i*1)
-    return product
-user_input=int(input("enter the number:"))
-factorial_of_user_input = factorial(user_input)
-print (factorial_of_user_input)
-
-
-print("Factorial is:")
-
+def factorial(n):
+ if n==1:
+     return 1
+ else:
+     return n * factorial(n-1)
+print(factorial(5))
+dict={}
+dict[5]=factorial(5)
+dict[4]=factorial(4)
+dict[3]=factorial(3)
+print(dict)
 
 #find the perfect number
 
 
-def perfect (n):
+def perfect(n):
  sum=0
  for i in range(1,n):
     if n%i==0:
       sum=sum+i
-    if sum==n:
-      return True
-    else:
-        return False
+ if sum==n:
+    return True
+ else:
+    return False
 # calling method in for loop
 for i in range(1,1000):
-    if perfect(i):
-        print(i)
+ if perfect(i):
+  print(i)
+
+ # print  table 0f 12
+
+def table(n):
+ n=12
+ for i in range(1,11):
+     print(n,'x',i,'=',n*i)
+table(12)
